a21ea64b6f5ad74e3c34c6ad32c9c4be155419de
unknown <jouga@jojonomic.com>
2024-05-19 02:08:35 +0700

    add getCountDown

--- dist/assets/ssrBoot-D_FVZOFH.js
+++
@@ -1,1 +0,0 @@
-import{a9 as t,g as e,Y as s,T as n}from"./index-DLwIUVuh.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
