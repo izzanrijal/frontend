383b9d2969f9fcde19b0d0a327a596bb59222a93
unknown <jouga@jojonomic.com>
2024-05-20 18:25:43 +0700

    add router in dashboard question

---
+++ dist/assets/ssrBoot-Dzt-zlXc.js
@@ -0,0 +1,1 @@
+import{a9 as t,g as e,X as s,T as n}from"./index-DxqvI5rR.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
