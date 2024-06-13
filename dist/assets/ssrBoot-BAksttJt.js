24037a8df4ca412723f678cf54e5650563d35221
unknown <jouga@jojonomic.com>
2024-06-13 17:35:42 +0700

    change is_can_be_done to is_done in question packet

--- dist/assets/ssrBoot-BAksttJt.js
+++
@@ -1,1 +0,0 @@
-import{aa as t,g as e,a0 as s,T as n}from"./index-CLgyV2nF.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
---
+++ dist/assets/ssrBoot-BDH1yfc-.js
@@ -0,0 +1,1 @@
+import{aa as t,g as e,a0 as s,T as n}from"./index-Dygydmz8.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
