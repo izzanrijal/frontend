0366760c2b9c32ab641a4b41d8f0c1a6c2e8d72d
unknown <jouga@jojonomic.com>
2024-06-19 17:46:10 +0700

    change is current with current number in question order

--- src/views/question-packet/QuestionOrder.vue
+++ src/views/question-packet/QuestionOrder.vue
@@ -12,12 +12,10 @@ const routeQuestionPacketID = ref(null)
 const soal = ref(null)
 var token = localStorage.getItem('token');
 const errorMessage = ref(null)
-const currentNumber = ref(null)
+const currentNumber = ref(localStorage.getItem('number') || null); // Initialize with stored number or null
 
 const buttonsPerRow = 5;
 onMounted(async () => {
-  currentNumber.value = localStorage.getItem('number')
-  console.log("CURRENT NUMBER: ", currentNumber.value)
   await getOrderNumber()
 });
 
@@ -52,6 +50,8 @@ const getOrderNumber = async () => {
         return result;
       });
       console.log("GROUP BUT: ", groupedButtons);
+      currentNumber.value = localStorage.getItem('number')
+      console.log("CURRENT NUMBER: ", currentNumber.value)
     } catch (error) {
       if (error.response && error.response.status === 401) {
         // Redirect to login page if the response status is 401
@@ -173,7 +173,7 @@ const nextPage = async () => {
               :key="item.number" 
               class="d-flex flex-wrap v-item">
               <VBtn
-                :color="item.number === currentNumber.value ? 'primary' : (item.is_fill ? '#0080ff' : '')"
+                :color="currentNumber.value == item.number ? 'primary' : (item.is_fill ? '#0080ff' : '')"
                 :variant="item.is_fill ? 'tonal' : 'outlined'"
                 class="mb-1"
                 style=" block-size: 40px;inline-size: 40px;"
