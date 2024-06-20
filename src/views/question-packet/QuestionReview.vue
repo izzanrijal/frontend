4ef98fa3e6f8428f804e99ac39c184cea864f981
unknown <jouga@jojonomic.com>
2024-06-20 12:19:15 +0700

    add border radius and marging

--- src/views/question-packet/QuestionReview.vue
+++ src/views/question-packet/QuestionReview.vue
@@ -188,6 +188,8 @@ const handleHover = (index, isHover) => {
 <style lang="scss" scoped>
 .outlined-card-item {
   border: 1px solid #ccc;
+  border-radius: 5px;
+  margin: 10px;
   cursor: pointer;
   transition: background-color 0.3s, border-color 0.3s;
 }
