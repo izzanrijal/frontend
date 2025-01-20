c465e2fc90e2405f8225864c96ca8b3111563722
unknown <jouga@jojonomic.com>
2025-01-20 23:25:49 +0700

    add image thumbnail for daftar/topik in new user

--- src/views/dashboard/GetTopicToLearn.vue
+++ src/views/dashboard/GetTopicToLearn.vue
@@ -1,13 +1,14 @@
 <script setup>
 import axios from 'axios';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router';
 
- // Replace with the actual key you use for the token
-var topicLists = ref([]);
-var token = localStorage.getItem('token');
+const topicLists = ref([]);
+const token = localStorage.getItem('token');
+const router = useRouter();
 
 onMounted(async () => {
-  getTopicToLearn()
+  getTopicToLearn();
 });
 
 const getTopicToLearn = async () => {
@@ -18,8 +19,7 @@ const getTopicToLearn = async () => {
           Authorization: `Bearer ${token}`,
         },
       });
-      topicLists.value = response.data.data
-      
+      topicLists.value = response.data.data;
     } catch (error) {
       if (error.response && error.response.status === 401) {
         // Redirect to login page if the response status is 401
@@ -38,12 +38,27 @@ const getTopicToLearn = async () => {
 <template>
   <VCard>
     <VRow no-gutters>
-      <VCol
-        cols="12"
-        md="12"
-      >
+      <VCol cols="12" md="12">
         <VCardItem>
-          <VCardTitle><span style="color: #005BC5;">Daftar Topik/Penyakit Prioritas</span></VCardTitle>
+          <VCardTitle>
+            <span style="color: #005BC5;">Daftar Topik/Penyakit Prioritas</span>
+          </VCardTitle>
+        </VCardItem>
+
+        <VCardText v-if="topicLists.length > 0">
+          <VList class="card-list">
+            <VListItem
+              v-for="(topic, index) in topicLists.slice(0, 5)"
+              :key="index"
+            >
+              <VListItemTitle class="font-weight-medium mb-1 custom-title-style">
+                <span style="color: #005BC5;">{{ topic.title }}</span>
+              </VListItemTitle>
+              <VListItemSubtitle class="text-body-1">
+                <span style="color: #005BC5;">{{ topic.subtitle }}</span>
+              </VListItemSubtitle>
+            </VListItem>
+          </VList>
 
           <template #append>
             <h6 class="text-h6">
@@ -53,23 +68,16 @@ const getTopicToLearn = async () => {
               >View All</a>
             </h6>
           </template>
-        </VCardItem>
+        </VCardText>
 
-        <VCardText>
-          <VList class="card-list">
-            <VListItem
-              v-for="(topic, index) in topicLists.slice(0, 5)"
-              :key="index"
-            >
-              
-              <VListItemTitle class="font-weight-medium mb-1 custom-title-style">
-                <span style="color: #005BC5;">{{ topic.title }}</span>
-              </VListItemTitle>
-              <VListItemSubtitle class="text-body-1">
-                <span style="color: #005BC5;">{{ topic.subtitle }}</span>
-              </VListItemSubtitle>
-            </VListItem>
-          </VList>
+        <VCardText v-else>
+          <div class="no-record-container">
+            <img
+              src="@/assets/images/topic_no_record.png"
+              alt="No records available"
+              class="no-record-image"
+            />
+          </div>
         </VCardText>
       </VCol>
     </VRow>
@@ -83,5 +91,18 @@ const getTopicToLearn = async () => {
 
 .custom-title-style {
   margin-block-end: 0 !important;
+}
+
+.no-record-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 50px;
+  block-size: 200px; /* Adjust height as needed */
+}
+
+.no-record-image {
+  block-size: auto;
+  max-inline-size: 100%;
 }
 </style>
