6d5ff267e07e132da9d281653bc2774d5d2adf12
izzanrijal <theprotectorboy@gmail.com>
2025-04-17 20:59:34 +0800

    add base to vite config

--- server.cjs
+++ server.cjs
@@ -1,18 +1,8 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-const PORT = process.env.PORT || 5050;
-const HOST = process.env.HOST || 'app.berkompeten.id';
-
-// Serve static files from the dist directory
 app.use(express.static(path.join(__dirname, 'dist')));
-
-// Handle all routes by serving the index.html file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://${HOST}:${PORT}/`);
-}); 
+app.listen(process.env.PORT || 5050);
